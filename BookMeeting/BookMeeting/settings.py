00939d4ef3da23b5bb6121107f83996d01d35539
huynam53 <106823781+huynam53@users.noreply.github.com>
2023-02-02 09:09:33 +0700

    Update settings.py

--- BookMeeting/BookMeeting/settings.py
+++ BookMeeting/BookMeeting/settings.py
@@ -219,9 +219,11 @@ SIMPLE_JWT = {
 
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # Email settings
+SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY")
+
 EMAIL_HOST = "smtp.sendgrid.net"
 EMAIL_HOST_USER = "apikey"  # this is exactly the value 'apikey'
-EMAIL_HOST_PASSWORD = "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"
+EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
